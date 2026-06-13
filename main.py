85727e70b22af0dfc8cf5aa38a02da27844081ca
Danya002 <annenkovdaniil58@gmail.com>
2026-06-13 03:25:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,20 @@ CHAT_ID = os.environ["CHAT_ID"]
 url = "https://bloxinformer.com/blox-fruits-stock/"
 response = requests.get(url)
 
-text = response.text[:3000]
+text = response.text
+
+# Ищем слово Fruit
+pos = text.find("Fruit")
+
+if pos != -1:
+    message = text[pos:pos+3000]
+else:
+    message = "Слово Fruit не найдено"
 
 requests.post(
     f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
     data={
         "chat_id": CHAT_ID,
-        "text": text
+        "text": message
     }
 )
