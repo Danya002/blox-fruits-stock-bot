9b73d7f36b1d1615aef830d116ef5bdf7fc4f4be
Danya002 <annenkovdaniil58@gmail.com>
2026-06-13 03:43:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,17 @@ import requests
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-url = "https://bloxfruitsvalues.com/stock"
+url = "https://bloxfruitscode.com/blox-fruits-stock-live-right-now/"
 response = requests.get(url)
 
 text = response.text
 
-# Ищем первое вхождение слова "Kitsune"
-pos = text.find("Kitsune")
+pos = text.find("Normal Stock")
 
 if pos != -1:
-    message = text[pos-500:pos+2000]
+    message = text[pos:pos+2500]
 else:
-    message = "Kitsune не найден"
+    message = "Normal Stock не найден"
 
 requests.post(
     f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
