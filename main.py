c45f30b086a91e3774e08d49853b0c7507b72cf0
Danya002 <annenkovdaniil58@gmail.com>
2026-06-13 03:29:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,13 @@ import requests
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-url = "https://bloxinformer.com/blox-fruits-stock/"
+url = "https://bloxfruitsvalues.com/stock"
 response = requests.get(url)
-
-text = response.text
-
-# Ищем слово Fruit
-pos = text.find("Fruit")
-
-if pos != -1:
-    message = text[pos:pos+3000]
-else:
-    message = "Слово Fruit не найдено"
 
 requests.post(
     f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
     data={
         "chat_id": CHAT_ID,
-        "text": message
+        "text": response.text[:3000]
     }
 )
