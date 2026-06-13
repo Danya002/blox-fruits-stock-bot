55bd80ddf78799c0d30c1e0871e6663df50b78f8
Danya002 <annenkovdaniil58@gmail.com>
2026-06-13 03:06:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import os
+import requests
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-print("Бот запущен!")
-print(BOT_TOKEN)
-print(CHAT_ID)
+# Получаем страницу со стоком
+url = "https://bloxinformer.com/blox-fruits-stock/"
+html = requests.get(url).text
+
+# Отправляем первые 4000 символов страницы (для проверки)
+message = html[:4000]
+
+requests.post(
+    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
